b6565cfc34be793f352c4255102bdb1346e79a00
FazliddinFayziev <fazrez4515@gmail.com>
2023-06-25 16:15:28 +0800

    first commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,20 @@
-export const schemaTypes = []
+import works from "./works"
+import skills from "./skills";
+import brands from "./brands";
+import abouts from "./abouts";
+import contact from "./contact";
+import experiences from "./experiences";
+import testimonials from "./testimonials";
+import workExperience from "./workExperience";
+
+
+export const schemaTypes = [
+    works,
+    skills,
+    brands,
+    abouts,
+    contact,
+    experiences,
+    testimonials,
+    workExperience,
+]
